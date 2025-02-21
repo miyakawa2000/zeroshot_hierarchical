83af7322978d0244de77610de27ea4ff2db38fb0
miyakawa <miyakawa.ryohei@ist.osaka-u.ac.jp>
2025-02-21 08:05:13 +0000

    add .sh files

--- docker/docker_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-username=$(id -u -n)
-container_name="${username}_zeroshot-hierarchical"
-docker run --gpus all -it -d \
-        --shm-size=50G \
-        --name "$container_name" \
-	-u $(id -u $username):$(id -g $username) \
-        -v /mnt/workspace2024/${username}/zeroshot_hierarchical:/home/${username}/mnt/workspace/zeroshot_hierarchical \
-        repo-luna.ist.osaka-u.ac.jp:5000/${username}/zeroshot-hierarchical:latest bash
